0dc5c58b430c63db4e98e6b7f124acd77f9575df
Mayar-ashraf8 <mayarashraf742@gmail.com>
2026-01-17 12:25:46 +0200

    update:the connection with database

--- src/connectdb.php
+++ src/connectdb.php
@@ -1,5 +1,5 @@
 <?php
-$connection = new mysqli("host.docker.internal", "root", "", "dbproject");
+$connection = new mysqli("db", "root", "", "dbproject");
 
 if ($connection->connect_error) {
     die("error: " . $connection->connect_error);
